714b7ec9422b909a75961e53a17810bb301bc3e0
pancake <pancake@nowsecure.com>
2026-03-25 08:47:04 +0100

    Fix sandbox canonicalization to build on Windows

--- src/path.inc.c
+++ src/path.inc.c
@@ -2,6 +2,10 @@
 
 // TODO: move this code into r2's util api for portability reasons
 
+#if R2__WINDOWS__
+#include <windows.h>
+#endif
+
 static bool r2mcp_path_contains_parent_ref(const char *path) {
 	R_RETURN_VAL_IF_FAIL (path, false);
 	const char *p = path;
@@ -16,17 +20,60 @@ static bool r2mcp_path_contains_parent_ref(const char *path) {
 	return false;
 }
 
+#if R2__WINDOWS__
+static char *r2mcp_path_realpath(const char *path) {
+	R_RETURN_VAL_IF_FAIL (path, NULL);
+	HANDLE h = CreateFileA (path, 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	if (h == INVALID_HANDLE_VALUE) {
+		return NULL;
+	}
+	DWORD n = GetFinalPathNameByHandleA (h, NULL, 0, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
+	if (!n) {
+		CloseHandle (h);
+		return NULL;
+	}
+	char *buf = malloc ((size_t)n + 1);
+	if (!buf) {
+		CloseHandle (h);
+		return NULL;
+	}
+	DWORD got = GetFinalPathNameByHandleA (h, buf, n, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
+	CloseHandle (h);
+	if (!got) {
+		free (buf);
+		return NULL;
+	}
+	buf[got] = '\0';
+	if (r_str_startswith (buf, "\\\\?\\UNC\\")) {
+		char *ret = r_str_newf ("\\\\%s", buf + 8);
+		free (buf);
+		return ret;
+	}
+	if (r_str_startswith (buf, "\\\\?\\")) {
+		char *ret = strdup (buf + 4);
+		free (buf);
+		return ret;
+	}
+	return buf;
+}
+#else
+static char *r2mcp_path_realpath(const char *path) {
+	R_RETURN_VAL_IF_FAIL (path, NULL);
+	return realpath (path, NULL);
+}
+#endif
+
 static bool r2mcp_path_is_within_sandbox(const char *path, const char *sandbox) {
 	if (R_STR_ISEMPTY (sandbox)) {
 		return true;
 	}
 	R_RETURN_VAL_IF_FAIL (path, false);
-	char *rp = realpath (path, NULL);
+	char *rp = r2mcp_path_realpath (path);
 	if (!rp) {
 		R_LOG_ERROR ("Access denied: unable to resolve path");
 		return false;
 	}
-	char *rs = realpath (sandbox, NULL);
+	char *rs = r2mcp_path_realpath (sandbox);
 	if (!rs) {
 		R_LOG_ERROR ("Access denied: unable to resolve sandbox path");
 		free (rp);
